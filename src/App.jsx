8596fb548e4aa4550d2647c3dc1d9b75b48f4b3a
mspark9 <stellaaa0906@gmail.com>
2026-02-24 17:14:01 +0900

    Implement a new Works section with GSAP animations for project previews and add `@iconify/react` dependency.

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Hero from './sections/Hero'
 import ServiceSummery from './sections/ServiceSummery'
 import Services from './sections/Services'
 import About from './sections/About'
+import Works from './sections/Works'
 
 const App = () => {
     return (
@@ -14,6 +15,7 @@ const App = () => {
             <ServiceSummery />
             <Services />
             <About />
+            <Works />
             {/* <section id='home' className='h-screen bg-amber-500'></section> */}
             <section id='services' className='h-screen bg-indigo-500'></section>
             <section id='about' className='h-screen bg-gray-500'></section>
